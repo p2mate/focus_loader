862081a1096e4f59284237e9e2a1296b0aff62f0
Peter De Schrijver <p2@psychaos.be>
2022-02-27 01:08:44 +0200

    only restore register when modified
    Signed-off-by: Peter De Schrijver <p2@psychaos.be>

--- int10_override.asm
+++ int10_override.asm
@@ -5,8 +5,6 @@
 
 int10_override_	proc	far
 
-	push	cx
-	push	bx
 	cmp 	ax, 4f01h
 	je	check_mode01
 	cmp	ax, 4f02h
@@ -14,22 +12,31 @@ int10_override_	proc	far
 
 	cmp	bx, 4136h
 	jne	chain_to_prev
+
+	push	cx
+	push	bx
 	and	bx, 4000h
 	or	bx, [mode]
-	jmp	chain_to_prev
+	jmp	chain_to_prev_and_restore
 
 check_mode01:
 	cmp	cx, 136h
 	jne	chain_to_prev
+
+	push	cx
+	push	bx
 	and	cx, 4000h
 	or	cx, [mode]
 
-chain_to_prev:	
+chain_to_prev_and_restore:
 	pushf
 	call	[cs:chain_int10]
 	pop	bx
 	pop	cx
 	iret
+
+chain_to_prev:	
+	jmp	[cs:chain_int10]
 
 int10_override_	endp
 
